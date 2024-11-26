f259e32a6493eef3703b95c994c8eeb235efe853
Bruno Peres <brunoperes7@hotmail.com>
2024-11-26 19:55:06 +0000

    fix: Daniel example

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import spacy
 nlp = spacy.load("pt_core_news_sm")
 sentence = "Emma Watson postou uma foto com Daniel Radcliffe."
 doc = nlp(sentence)
-for chunk in doc.noun.chunks:
+for chunk in doc.noun_chunks:
     print(chunk.text)
