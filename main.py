a21e59758f189c14bd9e7ceaaa3d3bdbac9b14d0
Bruno Peres <brunoperes7@hotmail.com>
2024-11-26 23:24:31 +0000

    original-example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import spacy
 nlp = spacy.load("pt_core_news_sm")
-sentence = "Emma Watson postou uma foto com Daniel Radcliffe."
+sentence = "Emma Watson postou uma foto com Daniel Radcliffe no Instagram, mas a imagem foi removida."
 doc = nlp(sentence)
 for chunk in doc.noun_chunks:
     print(chunk.text)
